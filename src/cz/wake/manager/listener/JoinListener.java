dbe181168070af9be2749ce30bcd4b6f3c5c66e4
MrWakeCZ <joseff.kral@gmail.com>
2016-05-20 23:26:08 +0200

    [1.0.1] Oprava při odpojení

--- src/cz/wake/manager/listener/JoinListener.java
+++ src/cz/wake/manager/listener/JoinListener.java
@@ -7,8 +7,11 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import cz.wake.manager.Main;
+import cz.wake.manager.particles.ParticlesAPI;
 
 public class JoinListener implements Listener{
+	
+	ParticlesAPI partAPI = new ParticlesAPI();
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e){
@@ -22,6 +25,7 @@ public class JoinListener implements Listener{
 	public void onQuit(PlayerQuitEvent e){
 		final Player p = e.getPlayer();
 		if(Main.getInstance().isVisibleForPlayer(p)){
+			partAPI.deactivateParticles(p);
 			Main.getInstance().removePlayer(p);
 		}
 	}
