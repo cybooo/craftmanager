1868409fb59bce13f04f3e09397eeb11e06a96fd
MrWakeCZ <joseff.kral@gmail.com>
2016-06-08 17:54:23 +0200

    [1.0.2] Odebrání SuperbVote

--- src/cz/wake/manager/votifier/SuperbVote.java
+++ src/cz/wake/manager/votifier/SuperbVote.java
@@ -2,24 +2,12 @@ package cz.wake.manager.votifier;
 
 import com.vexsoftware.votifier.model.VotifierEvent;
 import cz.wake.manager.Main;
-import io.minimum.minecraft.superbvote.votes.SuperbPreVoteEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class SuperbVote implements Listener{
-
-    @EventHandler
-    public void voteListener(SuperbPreVoteEvent e){
-        if(e.getResult() == SuperbPreVoteEvent.Result.QUEUE_VOTE){
-            try{
-                e.setResult(SuperbPreVoteEvent.Result.CANCEL);
-            } catch (NullPointerException ec){
-                ec.printStackTrace();
-            }
-        }
-    }
 
     @EventHandler
     public void voteSQL(final VotifierEvent e){
