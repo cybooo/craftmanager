f726a08e7c4d7bee7c3ecca07b95661487895736
MrWakeCZ <joseff.kral@gmail.com>
2018-02-17 00:08:32 +0100

    Úprava příkazu /skull

--- src/main/java/cz/wake/manager/perks/general/SkullCommand.java
+++ src/main/java/cz/wake/manager/perks/general/SkullCommand.java
@@ -1,47 +1,46 @@
 package cz.wake.manager.perks.general;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
 import cz.wake.manager.utils.ItemFactory;
-import net.minecraft.server.v1_12_R1.EntityPlayer;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SkullCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender Sender, Command Command, String String, String[] ArrayOfString) {
+    public boolean onCommand(CommandSender Sender, Command Command, String String, String[] array) {
         if (Sender instanceof Player) {
             Player player = (Player) Sender;
-            if ((Command.getName().equalsIgnoreCase("skull"))) {
-                if (player.hasPermission("craftmanager.develop")) {
-                    String[] name = getFromName(player.getName());
-//                  MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
-//                  GameProfile profile = new GameProfile(UUID.randomUUID(), player.getName());
-//                  profile.getProperties().put("textures", new Property("textures", name[0], name[1]));
+            if (Command.getName().equalsIgnoreCase("skull")) {
+                if (player.hasPermission("craftmanager.vip.skull")) {
 
-                    ItemStack head = ItemFactory.createHead("§e" + player.getName(), name[0], name[1]);
+                    String name, uuid, textureData;
 
-                    final Inventory inventory = (Inventory) player.getInventory();
-                    if (inventory.firstEmpty() != -1) {
-                        inventory.addItem(new ItemStack[]{head});
-                        player.sendMessage("§eTvoje headka ti byla pridana do inventare!");
-                    } else {
-                        final Location loc = player.getLocation();
-                        player.getWorld().dropItem(loc, head);
-                        player.sendMessage("§cNemas volne misto v inventari, headka lezi na zemi!");
+                    OkHttpClient caller = new OkHttpClient();
+                    Request request = new Request.Builder().url("https://api.mojang.com/users/profiles/minecraft/" + player.getName()).build();
+                    try {
+                        System.out.println("CALL: " + request.body());
+                        Response response = caller.newCall(request).execute();
+                        JSONObject json = new JSONObject(response.body().string());
+                        System.out.println("RESPONSE: " + json.toString());
+
+                        uuid = (String) json.get("id");
+                        giveHead(player, uuid);
+
+
+                    } catch (Exception e){
+                        player.sendMessage("§cChyba v API Mojangu! Zkus to znova zachvilku! :)");
+                        //e.printStackTrace();
                     }
                 }
                 return true;
@@ -50,32 +49,45 @@ public class SkullCommand implements CommandExecutor {
         return false;
     }
 
-    public String[] getFromPlayer(Player playerBukkit) {
-        EntityPlayer playerNMS = ((CraftPlayer) playerBukkit).getHandle();
-        GameProfile profile = playerNMS.getProfile();
-        Property property = profile.getProperties().get("textures").iterator().next();
-        String texture = property.getValue();
-        String signature = property.getSignature();
-        return new String[]{texture, signature};
+    private void giveHead(Player p, String uuid){
+        String value, formatedUUID;
+        OkHttpClient caller2 = new OkHttpClient();
+        Request request2 = new Request.Builder().url("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid).build();
+        try {
+            System.out.println("CALL-1: " + request2.body());
+            Response response = caller2.newCall(request2).execute();
+            JSONObject json = new JSONObject(response.body().string());
+            System.out.println("RESPONSE: " + json.toString());
+            JSONArray jsonArray = json.getJSONArray("properties");
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            value = (String) jsonObject.get("value");
+
+            formatedUUID = addUUIDDashes(uuid);
+
+            ItemStack head = ItemFactory.createHead(p.getName(), formatedUUID, value);
+
+            final Inventory inventory = p.getInventory();
+            if (inventory.firstEmpty() != -1) {
+                inventory.addItem(head);
+                p.sendMessage("§eTvoje headka ti byla pridana do inventare!");
+            } else {
+                final Location loc = p.getLocation();
+                p.getWorld().dropItem(loc, head);
+                p.sendMessage("§cNemas volne misto v inventari, headka lezi na zemi!");
+            }
+
+        } catch (Exception e){
+            p.sendMessage("§cChyba v API Mojangu! Zkus to znova zachvilku! :)");
+            //e.printStackTrace();
+        }
     }
 
-    public String[] getFromName(String name) {
-        try {
-            URL url_0 = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
-            InputStreamReader reader_0 = new InputStreamReader(url_0.openStream());
-            String uuid = new JsonParser().parse(reader_0).getAsJsonObject().get("id").getAsString();
-
-            URL url_1 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
-            InputStreamReader reader_1 = new InputStreamReader(url_1.openStream());
-            JsonObject textureProperty = new JsonParser().parse(reader_1).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
-            String texture = textureProperty.get("value").getAsString();
-            String signature = textureProperty.get("signature").getAsString();
-
-            return new String[]{uuid, texture, signature};
-        } catch (IOException e) {
-            System.err.println("Could not get skin data from session servers!");
-            e.printStackTrace();
-            return null;
-        }
+    public static String addUUIDDashes(String idNoDashes) {
+        StringBuffer idBuff = new StringBuffer(idNoDashes);
+        idBuff.insert(20, '-');
+        idBuff.insert(16, '-');
+        idBuff.insert(12, '-');
+        idBuff.insert(8, '-');
+        return idBuff.toString();
     }
 }
