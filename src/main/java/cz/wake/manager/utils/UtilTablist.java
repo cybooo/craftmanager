ac3e1a9b20dc07c8cc2e7eef9c950e159c1280ca
MrWakeCZ <joseff.kral@gmail.com>
2018-07-01 04:28:05 +0200

    Úprava tablistu podle nových ranků

--- src/main/java/cz/wake/manager/utils/UtilTablist.java
+++ src/main/java/cz/wake/manager/utils/UtilTablist.java
@@ -6,23 +6,10 @@ import org.bukkit.entity.Player;
 public class UtilTablist {
 
     public static void setupTablist(final Player p) {
-        if (Main.getInstance().getConfig().getString("server").equalsIgnoreCase("creative")
-                || Main.getInstance().getConfig().getString("server").equalsIgnoreCase("creative2")) {
-            setupPrefixInTabCreative(p);
-        } else if (Main.getInstance().getConfig().getString("server").equalsIgnoreCase("survival")) {
-            setupPrefixInTabSurvival(p);
-        } else if (Main.getInstance().getConfig().getString("server").equalsIgnoreCase("skyblock")) {
-            setupPrefixInTabSkyblock(p);
-        } else if (Main.getInstance().getConfig().getString("server").equalsIgnoreCase("vanilla")) {
-            setupPrefixInTabVanilla(p);
-        } else if (Main.getInstance().getConfig().getString("server").equalsIgnoreCase("vanillasb")) {
-            setupPrefixInTabCreative(p);
-        } else {
-            setupPrefixInTabSurvival(p); //Default
-        }
+        setupDefaultTablist(p);
     }
 
-    private static void setupPrefixInTabSkyblock(final Player p) {
+    private static void setupDefaultTablist(final Player p) {
         if (p.hasPermission("craftmanager.prefix.majitel")) {
             p.setPlayerListName("§3§lMAJITEL §f" + p.getName());
         } else if (p.hasPermission("craftmanager.prefix.hladmin")) {
@@ -35,116 +22,22 @@ public class UtilTablist {
             p.setPlayerListName("§d§lEVENTER §f" + p.getName());
         } else if (p.hasPermission("craftmanager.prefix.developer")) {
             p.setPlayerListName("§e§lDEVELOPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.admin")) {
-            p.setPlayerListName("§c§lADMIN §f" + p.getName());
         } else if (p.hasPermission("craftmanager.prefix.builder")) {
             p.setPlayerListName("§1§lBUILDER §f" + p.getName());
+        } else if (p.hasPermission("craftmanager.prefix.admin")) {
+            p.setPlayerListName("§c§lADMIN §f" + p.getName());
         } else if (p.hasPermission("craftmanager.prefix.helperka")) {
             p.setPlayerListName("§2§lHELPERKA §f" + p.getName());
         } else if (p.hasPermission("craftmanager.prefix.helper")) {
             p.setPlayerListName("§2§lHELPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hero")) {
-            p.setPlayerListName("§5§lHERO §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.dvip")) {
-            p.setPlayerListName("§b§lDIAMOND §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.gvip")) {
-            p.setPlayerListName("§6§lGOLD §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.ivip")) {
-            p.setPlayerListName("§8§lIRON §f" + p.getName());
-        } else {
-            p.setPlayerListName("§f" + p.getName());
-        }
-    }
-
-    private static void setupPrefixInTabSurvival(final Player p) {
-        if (p.hasPermission("craftmanager.prefix.majitel")) {
-            p.setPlayerListName("§3§lMAJITEL §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hladmin")) {
-            p.setPlayerListName("§c§lHL.ADMIN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hlbuilder")) {
-            p.setPlayerListName("§1§lHL.BUILDER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.adminka")) {
-            p.setPlayerListName("§c§lADMINKA §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.eventer")) {
-            p.setPlayerListName("§d§lEVENTER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.developer")) {
-            p.setPlayerListName("§e§lDEVELOPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.admin")) {
-            p.setPlayerListName("§c§lADMIN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.builder")) {
-            p.setPlayerListName("§1§lBUILDER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.helperka")) {
-            p.setPlayerListName("§2§lHELPERKA §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.helper")) {
-            p.setPlayerListName("§2§lHELPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hero")) {
-            p.setPlayerListName("§5§lHERO §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.dvip")) {
+        } else if (p.hasPermission("craftmanager.prefix.obsidian")) {
             p.setPlayerListName("§9§lOBSIDIAN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.ovip")) {
+        } else if (p.hasPermission("craftmanager.prefix.emerald")) {
+            p.setPlayerListName("§a§lEMERALD §f" + p.getName());
+        } else if (p.hasPermission("craftmanager.prefix.diamond")) {
             p.setPlayerListName("§b§lDIAMOND §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.gvip")) {
+        } else if (p.hasPermission("craftmanager.prefix.gold")) {
             p.setPlayerListName("§6§lGOLD §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.ivip")) {
-            p.setPlayerListName("§8§lIRON §f" + p.getName());
-        } else {
-            p.setPlayerListName("§f" + p.getName());
-        }
-    }
-
-    private static void setupPrefixInTabCreative(final Player p) {
-        if (p.hasPermission("craftmanager.prefix.majitel")) {
-            p.setPlayerListName("§3§lMAJITEL §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hladmin")) {
-            p.setPlayerListName("§c§lHL.ADMIN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hlbuilder")) {
-            p.setPlayerListName("§1§lHL.BUILDER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.adminka")) {
-            p.setPlayerListName("§c§lADMINKA §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.eventer")) {
-            p.setPlayerListName("§d§lEVENTER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.developer")) {
-            p.setPlayerListName("§e§lDEVELOPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.admin")) {
-            p.setPlayerListName("§c§lADMIN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.builder")) {
-            p.setPlayerListName("§1§lBUILDER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.helperka")) {
-            p.setPlayerListName("§2§lHELPERKA §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.helper")) {
-            p.setPlayerListName("§2§lHELPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hero")) {
-            p.setPlayerListName("§5§lHERO §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.vipplus")) {
-            p.setPlayerListName("§a§lVIP+ §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.vip")) {
-            p.setPlayerListName("§6§lVIP §f" + p.getName());
-        } else {
-            p.setPlayerListName("§f" + p.getName());
-        }
-    }
-
-    private static void setupPrefixInTabVanilla(final Player p) {
-        if (p.hasPermission("craftmanager.prefix.majitel")) {
-            p.setPlayerListName("§3§lMAJITEL §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hladmin")) {
-            p.setPlayerListName("§c§lHL.ADMIN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.hlbuilder")) {
-            p.setPlayerListName("§1§lHL.BUILDER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.adminka")) {
-            p.setPlayerListName("§c§lADMINKA §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.eventer")) {
-            p.setPlayerListName("§d§lEVENTER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.developer")) {
-            p.setPlayerListName("§e§lDEVELOPER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.admin")) {
-            p.setPlayerListName("§c§lADMIN §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.builder")) {
-            p.setPlayerListName("§1§lBUILDER §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.helperka")) {
-            p.setPlayerListName("§2§lHELPERKA §f" + p.getName());
-        } else if (p.hasPermission("craftmanager.prefix.helper")) {
-            p.setPlayerListName("§2§lHELPER §f" + p.getName());
         } else {
             p.setPlayerListName("§f" + p.getName());
         }
