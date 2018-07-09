cfea24d0fa68b1fe925478da3a207eda4055f173
MrWakeCZ <joseff.kral@gmail.com>
2018-07-10 01:52:08 +0200

    Info o novém hlasování...

--- src/main/java/cz/wake/manager/votifier/ForwardVote.java
+++ src/main/java/cz/wake/manager/votifier/ForwardVote.java
@@ -8,11 +8,15 @@ import org.bukkit.entity.Player;
 public class ForwardVote {
 
     public static void vote(final String nick, final String uuid, final String coins) {
-        Bukkit.getPluginManager().callEvent(new VotifierEvent(null)); // fake?
+        //Bukkit.getPluginManager().callEvent(new VotifierEvent(null)); // fake?
 
         Player player = Bukkit.getServer().getPlayer(nick);
 
-        Titles.sendFullTitlePlayer(player, 10, 60, 10, "§a§lDekujeme!", "§fDostal/a jsi odmenu.");
+        Titles.sendFullTitlePlayer(player, 10, 60, 10, "§a§lDekujeme!", "§fDostal/a jsi 1x VoteToken.");
+        player.sendMessage(" ");
+        player.sendMessage("§bNove si muzes vybrat, co chces jako odmenu!");
+        player.sendMessage("§eStaci zajit do §f/cshop §ea zvolit si odmenu za VoteTokeny.");
+        player.sendMessage("");
         for (Player p : Bukkit.getOnlinePlayers()) {
             p.sendMessage("§b" + player.getName() + " §ehlasoval a ziskal §aodmenu! §c/vote");
         }
