5e0375202119f35efb2b45bd76c479e2a71fbeae
MrWakeCZ <joseff.kral@gmail.com>
2018-10-03 23:14:58 +0200

    Dočasná oprava null při reminderu hlasování

--- src/main/java/cz/wake/manager/votifier/Reminder.java
+++ src/main/java/cz/wake/manager/votifier/Reminder.java
@@ -8,9 +8,10 @@ public class Reminder implements Runnable {
 
     @Override
     public void run() {
+        //TODO: Napojit pozdeji na CraftEconomy API
         for (Player p : Bukkit.getOnlinePlayers()) {
-            p.sendMessage("§cNezapomen hlasovat! Tento mesic mas §f" + Main.getInstance().getVoteHandler().getPlayerCachedMonthVotes(p) + " §chlasu! §8(Celkem: "
-                    + Main.getInstance().getVoteHandler().getPlayerCachedTotalVotes(p) + ")");
+            p.sendMessage("§cNezapomen hlasovat! Tento mesic mas §f" + Main.getInstance().getMySQL().getPlayerTotalMonth(p.getUniqueId()) + " §chlasu! §8(Celkem: "
+                    + Main.getInstance().getMySQL().getPlayerTotalVotes(p.getUniqueId()) + ")");
         }
     }
 }
