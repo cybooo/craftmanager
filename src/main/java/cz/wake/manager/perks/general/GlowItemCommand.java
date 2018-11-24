2091956f90c91c39f59317d5d3c79e8f6d2953c8
MrWakeCZ <joseff.kral@gmail.com>
2018-11-24 02:11:33 +0100

    Oprava příkazu /gi, který fungoval na vše

--- src/main/java/cz/wake/manager/perks/general/GlowItemCommand.java
+++ src/main/java/cz/wake/manager/perks/general/GlowItemCommand.java
@@ -1,6 +1,9 @@
 package cz.wake.manager.perks.general;
 
 import cz.craftmania.craftcore.spigot.builders.items.ItemBuilder;
+import cz.wake.manager.Main;
+import n3kas.ae.api.AEAPI;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,9 +23,33 @@ public class GlowItemCommand implements CommandExecutor {
                 ItemStack item = player.getInventory().getItemInMainHand();
                 if(item == null) {
                     player.sendMessage("§cMusis drzet item, na ktery chces dat glowing efekt.");
+                    return true;
                 }
+
+                if (item.getType() == Material.ENCHANTED_GOLDEN_APPLE) {
+                    player.sendMessage("§cNa tento item nelze pouzit prikaz /gi");
+                    return true;
+                }
+
+                if (item.getAmount() > 1) {
+                    player.sendMessage("§cGlowItem lze pouzit pouze na jeden item!");
+                    return true;
+                }
+
+                if (!item.getEnchantments().isEmpty()) {
+                    player.sendMessage("§cNelze pouzit GlowItem na item, ktery jiz ma enchant!");
+                    return true;
+                }
+
+                if (Main.getInstance().isCustomDisenchantEnabled()) {
+                    if (!AEAPI.getEnchantmentsOnItem(item).isEmpty()) {
+                        player.sendMessage("§cNelze pouzit GlowItem na item, ktery jiz ma enchant!");
+                        return true;
+                    }
+                }
+
                 ItemBuilder itemBuilder = new ItemBuilder(item);
-                player.getInventory().remove(item);
+                player.getInventory().setItemInMainHand(null);
                 itemBuilder.setGlowing();
                 itemBuilder.setAmount(1);
                 player.getInventory().setItemInMainHand(itemBuilder.build());
