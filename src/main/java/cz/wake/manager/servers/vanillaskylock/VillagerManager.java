ebb26ec1cfc8a9eb0a78cc10d956345e4081bb6b
MrWakeCZ <joseff.kral@gmail.com>
2019-07-01 17:58:35 +0200

    Přidán první výkup pro Villagery [test]

--- src/main/java/cz/wake/manager/servers/vanillaskylock/VillagerManager.java
+++ src/main/java/cz/wake/manager/servers/vanillaskylock/VillagerManager.java
@@ -2,6 +2,8 @@ package cz.wake.manager.servers.vanillaskylock;
 
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import cz.craftmania.craftcore.spigot.builders.items.CustomMaterial;
+import cz.craftmania.craftcore.spigot.builders.items.ItemBuilder;
 import cz.craftmania.craftcore.spigot.builders.villager.VillagerTradeBuilder;
 import cz.wake.manager.Main;
 import org.bukkit.Bukkit;
@@ -10,6 +12,8 @@ import org.bukkit.Material;
 import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,16 +65,68 @@ public class VillagerManager {
 
         VillagerTradeBuilder tradeBuilder = new VillagerTradeBuilder();
         tradeBuilder.clearTrades(villager);
-        tradeBuilder.addTrade(new ItemStack(Material.BREAD), new ItemStack(Material.DRAGON_EGG));
-        tradeBuilder.addTrade(new ItemStack(Material.DIAMOND), new ItemStack(Material.BREAD));
-        tradeBuilder.addTrade(new ItemStack(Material.CHICKEN), new ItemStack(Material.ACACIA_LOG));
-        tradeBuilder.addTrade(new ItemStack(Material.COOKED_BEEF), new ItemStack(Material.WHEAT));
-        tradeBuilder.addTrade(new ItemStack(Material.ANVIL), new ItemStack(Material.BAMBOO));
-        tradeBuilder.addTrade(new ItemStack(Material.ELYTRA), new ItemStack(Material.BLUE_DYE));
-        tradeBuilder.addTrade(new ItemStack(Material.BEDROCK), new ItemStack(Material.BEDROCK));
-        tradeBuilder.addTrade(new ItemStack(Material.BLACK_BED), new ItemStack(Material.BLUE_BED));
-        tradeBuilder.addTrade(new ItemStack(Material.CREEPER_HEAD), new ItemStack(Material.LAPIS_ORE));
-        tradeBuilder.addTrade(new ItemStack(Material.DRAGON_HEAD), new ItemStack(Material.PAPER));
+
+        // Farmareni
+        tradeBuilder.addTrade(new ItemStack(Material.CAKE), new ItemStack(Material.EMERALD));
+        tradeBuilder.addTrade(new ItemStack(Material.PUMPKIN_PIE), new ItemStack(Material.EMERALD));
+        tradeBuilder.addTrade(new ItemStack(Material.POISONOUS_POTATO, 16), new ItemStack(Material.EMERALD));
+        tradeBuilder.addTrade(new ItemStack(Material.BAKED_POTATO, 48), new ItemStack(Material.EMERALD));
+        tradeBuilder.addTrade(new ItemStack(Material.RABBIT_STEW), new ItemStack(Material.EMERALD));
+        tradeBuilder.addTrade(new ItemStack(Material.GLISTERING_MELON_SLICE, 5), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.GOLDEN_CARROT, 5), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE), new ItemStack(Material.EMERALD, 5));
+
+        // Drevo
+        tradeBuilder.addTrade(new ItemStack(Material.OAK_WOOD, 48), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.ACACIA_WOOD, 48), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.BIRCH_WOOD, 48), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.DARK_OAK_WOOD, 48), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.JUNGLE_WOOD, 48), new ItemStack(Material.EMERALD, 2));
+
+        // Redstone
+        tradeBuilder.addTrade(new ItemStack(Material.COMPARATOR, 32), new ItemStack(Material.EMERALD, 3));
+        tradeBuilder.addTrade(new ItemStack(Material.OBSERVER, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.COMPASS, 48), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.REPEATER, 48), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.STICKY_PISTON, 32), new ItemStack(Material.EMERALD, 4));
+        tradeBuilder.addTrade(new ItemStack(Material.DISPENSER, 42), new ItemStack(Material.EMERALD, 3));
+        tradeBuilder.addTrade(new ItemStack(Material.HOPPER, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.DAYLIGHT_DETECTOR, 24), new ItemStack(Material.EMERALD, 5));
+
+        // Ostatni
+        tradeBuilder.addTrade(new ItemStack(Material.CROSSBOW), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.ANVIL), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.WITHER_SKELETON_SKULL), new ItemStack(Material.EMERALD, 3));
+        tradeBuilder.addTrade(new ItemStack(Material.WRITABLE_BOOK), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.TURTLE_EGG, 16), new ItemStack(Material.EMERALD, 3));
+        tradeBuilder.addTrade(new ItemStack(Material.PACKED_ICE, 16), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.FIRE_CHARGE, 32), new ItemStack(Material.EMERALD, 4));
+        tradeBuilder.addTrade(new ItemStack(Material.NETHER_STAR, 24), new ItemStack(Material.EMERALD, 8));
+        tradeBuilder.addTrade(new ItemStack(Material.SLIME_BLOCK, 64), new ItemStack(Material.EMERALD, 3));
+        tradeBuilder.addTrade(new ItemStack(Material.MYCELIUM), new ItemStack(Material.EMERALD));
+        tradeBuilder.addTrade(new ItemStack(Material.CRACKED_STONE_BRICKS, 24), new ItemStack(Material.EMERALD, 1));
+
+        // Potions
+        //tradeBuilder.addTrade(new ItemBuilder(new CraftPotion(PotionBase.NORMAL, PotionType.INVISIBILITY, true, true).getItemStack()).setAmount(5).build(), new ItemStack(Material.EMERALD, 3));
+
+        // Terracota
+        tradeBuilder.addTrade(new ItemStack(Material.WHITE_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.ORANGE_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.MAGENTA_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.LIGHT_BLUE_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.YELLOW_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.LIME_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.PINK_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.GRAY_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.LIGHT_GRAY_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.CYAN_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.PURPLE_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.BLUE_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.BROWN_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.GREEN_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.RED_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+        tradeBuilder.addTrade(new ItemStack(Material.BLACK_GLAZED_TERRACOTTA, 32), new ItemStack(Material.EMERALD, 2));
+
         tradeBuilder.setTrades(villager);
 
         sellVillagerLocation.add(0, 3, 0);
@@ -185,4 +241,43 @@ public class VillagerManager {
     public static void setMetadata(WanderingTrader villager, String paramString, Object paramObject, Main paramMain) {
         villager.setMetadata(paramString, new FixedMetadataValue(paramMain, paramObject));
     }
+
+    public enum PotionBase {NORMAL, ARROW, LINGERING, SPLASH,}
 }
+
+class CraftPotion {
+    private static final String v = Bukkit.getVersion();
+    private final ItemStack potion;
+    private final Object potiondata;
+
+    public CraftPotion(VillagerManager.PotionBase base, PotionType type, boolean extended, boolean upgraded) {
+        final ItemStack is = new ItemStack(Material.matchMaterial(base.name().equals("NORMAL") ? "POTION" : base.name().equals("ARROW") ? v.contains("1.8") || v.contains("1.9") || v.contains("1.11") ? "ARROW" : "TIPPED_ARROW" : base.name() + "_POTION"));
+        final boolean a = !is.getType().equals(Material.ARROW);
+        org.bukkit.inventory.meta.PotionMeta pm = null;
+        org.bukkit.potion.PotionData pd = null;
+        if (a) {
+            pm = (org.bukkit.inventory.meta.PotionMeta) is.getItemMeta();
+            pd = new org.bukkit.potion.PotionData(type, type.isExtendable() ? extended : false, type.isUpgradeable() ? upgraded : false);
+        }
+        potiondata = pd;
+        if (a) {
+            pm.setBasePotionData(pd);
+            is.setItemMeta(pm);
+        }
+        potion = is;
+    }
+
+    public ItemStack getItemStack() {
+        return potion;
+    }
+
+    public ItemStack getItemStack(int amount){
+        potion.setAmount(amount);
+        return potion;
+    }
+
+    public Object getPotionData() {
+        return potiondata;
+    }
+}
+
