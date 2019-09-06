90f58f5c0b494e37c5d393b34e8f4e1825c335c4
MrWakeCZ <joseff.kral@gmail.com>
2019-09-07 00:36:57 +0200

    Informace o betě po připojení

--- src/main/java/cz/wake/manager/listener/PlayerListener.java
+++ src/main/java/cz/wake/manager/listener/PlayerListener.java
@@ -83,6 +83,16 @@ public class PlayerListener implements Listener {
         //Register Recipe Player
         RecipeManager.registerRecipePlayer(new RecipePlayer(p));
 
+        // Info o betě
+        if (Main.getServerType() == ServerType.CREATIVE) {
+            p.sendMessage("");
+            p.sendMessage("§c§lUpozornění");
+            p.sendMessage("§7Tento server je označený jako §bBETA§7!");
+            p.sendMessage("§7Mohou se zde vyskytovat bugy, lagy a jiné problémy.");
+            p.sendMessage("§7Budeme rádi, když nám dané problémy nahlásíš! :)");
+            p.sendMessage("");
+        }
+
         if (Main.restartReason != null) {
             Long min = Main.restartTime - System.currentTimeMillis();
             if (min > 60 * 60 * 1000) {
