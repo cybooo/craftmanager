c28fee81bb37d1386f2f290f12d61c018db8cbfc
Jacob Bordas <methalicity@gmail.com>
2019-09-18 19:57:20 +0200

    Check zda je hráč na cudzom ostrove, ak nie tak to nebude spamovať

--- src/main/java/cz/wake/manager/listener/DeathListener.java
+++ src/main/java/cz/wake/manager/listener/DeathListener.java
@@ -57,11 +57,16 @@ public class DeathListener implements Listener {
         if (e.getDamager() instanceof Guardian || e.getDamager() instanceof ElderGuardian) {
             if (e.getCause().equals(EntityDamageEvent.DamageCause.THORNS)) return;
         }
-        if (e.getDamager() instanceof Vindicator) return;
 
         if (e.getDamage() >= p.getHealth()) {
             if(e.isCancelled()) {
                 return;
+            }
+            if (Main.getInstance().getIdServer().equalsIgnoreCase("skyblock")) {
+                Island is = ASkyBlockAPI.getInstance().getIslandAt(p.getLocation());
+                if (is != null) {
+                    if (is != ASkyBlockAPI.getInstance().getIslandOwnedBy(p.getUniqueId())) return;
+                }
             }
             for (Player pl : Main.getInstance().death_messages) {
                 pl.sendMessage(Main.getInstance().getConfig().getStringList("d_msgs.mob").get(r.nextInt(Main.getInstance().getConfig().getStringList("d_msgs.mob").size()))
