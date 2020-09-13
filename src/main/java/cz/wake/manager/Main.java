be0175050c416194272fa4f70a8d178692720d33
MrWakeCZ <joseff.kral@gmail.com>
2020-09-13 23:09:01 +0200

    Odebrány příkazy pro teleport na 1.12 servery

--- src/main/java/cz/wake/manager/Main.java
+++ src/main/java/cz/wake/manager/Main.java
@@ -295,11 +295,9 @@ public class Main extends JavaPlugin implements PluginMessageListener {
 
         //Servers
         manager.registerCommand(new Survival_command());
-        manager.registerCommand(new Survival2_command());
         manager.registerCommand(new Skyblock_command());
-        manager.registerCommand(new Skyblock2_command());
         manager.registerCommand(new Creative_command());
-        manager.registerCommand(new Prison_command());
+        //manager.registerCommand(new Prison_command());
         manager.registerCommand(new Vanilla_command());
         manager.registerCommand(new Skycloud_command());
 
